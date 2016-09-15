257abe9aeadbbf417f9b189e26c44a71e67d5ea9
T-Brawl <t-brawl@hotmail.fr>
2016-09-15 23:06:22 +0200

    Color

--- wator/Ocean.py
+++ wator/Ocean.py
@@ -6,9 +6,9 @@ from core import Window
 class Ocean(Window.Window):
 
 	def __init__(self, gridSizeX,gridSizeY,canvasSizeX,canvasSizeY):
-		super(Ocean, self).__init__(gridSizeX,gridSizeY,canvasSizeX,canvasSizeY,boxSize=None,windowbg='blue',title='Simulation')
-		self.sharksStats = Canvas(self.tk, width =200, height =200, bg ='white')
-		self.shraksStats.pack(side=BOTTOM)
+		super(Ocean, self).__init__(gridSizeX,gridSizeY,canvasSizeX,canvasSizeY,boxSize=None,windowbg='#0077BE',title='Golfe du Bénin')
+		self.sharksStats = Canvas(self.tk, width =200, height =200, bg ='#E4229C')
+		self.sharksStats.pack(side=BOTTOM)
 
-		self.fishStats = Canvas(self.tk, width =200, height =200, bg ='grey')
+		self.fishStats = Canvas(self.tk, width =200, height =200, bg ='#5F9800')
 		self.fishStats.pack(side=RIGHT)
