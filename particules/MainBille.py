36076437e6bef4e7762672f92e8d5c4e316ad1ce
T-Brawl <t-brawl@hotmail.fr>
2016-09-15 15:13:04 +0200

    More parameters for the window

--- particules/MainBille.py
+++ particules/MainBille.py
@@ -1,4 +1,20 @@
 from core import SMA
 import BilleCreator as bc
 
-SMA.SMA(gridSizeX=100,gridSizeY=100,canvasSizeX=1920,canvasSizeY=1080,refresh=1,scheduling="",nbTicks=0,trace=True,grid=True,seed=None,delay=1,nbAgents=100,torique=False,agentCreator=bc.BilleCreator())
+SMA.SMA(gridSizeX=100
+    ,gridSizeY=100
+    ,canvasSizeX=1920
+    ,canvasSizeY=1080
+    ,refresh=1
+    ,scheduling=""
+    ,nbTicks=0
+    ,trace=True
+    ,grid=True
+    ,seed=None
+    ,delay=1
+    ,nbAgents=100
+    ,torique=False
+    ,agentCreator=bc.BilleCreator()
+    ,windowbg="white"
+    ,title="Simulation de particules"
+    )
